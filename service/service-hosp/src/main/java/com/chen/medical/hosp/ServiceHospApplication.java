ff1f92aad082266b9c7a447e2ce3f2ac011086b9
maybeBin <11017294@qq.com>
2023-05-10 23:16:22 +0800

    添加hosp配置

---
+++ service/service-hosp/src/main/java/com/chen/medical/hosp/ServiceHospApplication.java
@@ -0,0 +1,21 @@
+package com.chen.medical.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *  ServiceHosp启动类
+ * </p>
+ *
+ * @author：MaybeBin
+ * @Date: 2023-05-10 17:11
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+
+}
