58a8bc0154a80d1d96c4d6598b89623b793fe822
maybeBin <11017294@qq.com>
2023-05-22 10:56:28 +0800

    添加医院模拟系统

---
+++ hospital-manage/src/main/java/com/chen/hospital/config/MybatisPlusConfig.java
@@ -0,0 +1,31 @@
+package com.chen.hospital.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * MybatisPlus配置类
+ *
+ * @author MaybeBin
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.chen.hospital.mapper")
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 乐观锁插件
+        // interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        // 分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
