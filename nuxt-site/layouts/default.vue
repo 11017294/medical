8acf90099558282214641c3441e03616c959a499
maybeBin <11017294@qq.com>
2023-06-08 16:14:25 +0800

    医院前台系统

---
+++ nuxt-site/layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="app-container">
+      <div id="main">
+        <!-- 公共头 -->
+        <myheader/>
+        <div class="main-container">
+          <el-scrollbar class='page-component__scroll'>
+          <!-- 内容区域 -->
+          <nuxt/>
+          </el-scrollbar>
+        </div>
+          <!-- 公共底 -->
+        <myfooter/>
+      </div>
+    </div>
+</template>
+<script>
+import '~/assets/css/app.css'
+import '~/assets/css/chunk.css'
+import '~/assets/css/iconfont.css'
+import '~/assets/css/main.css'
+import Myfooter from "~/layouts/myfooter.vue";
+import myheader from "~/layouts/myheader.vue";
+export default {
+  components: { Myfooter, myheader },
+
+  mounted() {
+    window.document.getElementById("__nuxt").classList.add("app-container")
+    window.document.getElementById("__layout").classList.add("app-container")
+  }
+}
+</script>
